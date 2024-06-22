c9a2b78fa8fb162f94cb941c8c15cb1e91e6949c
Simon <73712759+ZvSimon@users.noreply.github.com>
2024-06-22 23:04:43 +0200

    Extacting values on Forms with ngModel

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
-
-
+  enteredInitialInvestment:string = '0';
+  enteredAnnualInterest:string = '0';
+  enteredExpectedReturn:string = '5';
+  enteredDuration:string = '10';
+  onSubmit(){
+console.log(this.enteredInitialInvestment);
+console.log(this.enteredAnnualInterest);
+console.log(this.enteredExpectedReturn);
+console.log(this.enteredDuration);
+  }
 }
